18c076d2b26336af842582ff92e9f1b4eb49ba3a
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-08-17 13:20:49 +0700

    basic list

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,6 +1,19 @@
 <script setup>
+  const list = [
+  {
+    name: 'First List',
+  },
+  {
+    name: 'Second List',
+  }
+];
 </script>
 
 <template>
- <h1>Test</h1>
+  <h1>Test</h1>
+  <ol>
+    <template v-for="item in list" v-bind:key="item">
+      <li>{{ item.name }}</li>
+    </template>
+  </ol>
 </template>
