4859b43157f0ee0814136655d4da8b5aac37dd09
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-08-19 13:52:06 +0700

    navigation guards, redirections, auth store, login

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@store/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,8 @@ const router = createRouter({
     },
     {
       path: '/profile',
+      name: 'Profile',
+      redirect: { name: 'Login' },
       children: [
         {
           path: '',
@@ -31,9 +34,12 @@ const router = createRouter({
           component: () => import('@/views/Profile/LoginView.vue')
         },
         {
-          path: 'detail/:id',
+          // add optional params
+          path: 'detail/:id?',
           name: 'Authenticated',
-          component: () => import('@/views/Profile/AuthenticatedView.vue')
+          component: () => import('@/views/Profile/AuthenticatedView.vue'),
+          // set protected route
+          meta: { auth: true }
         }
       ]
     },
@@ -43,6 +49,20 @@ const router = createRouter({
       component: () => import('@/views/NotFoundView.vue')
     }
   ]
+})
+
+// navigation guards
+router.beforeEach((to, from, next) => {
+  // get auth state
+  const loggedIn = useAuthStore().isLoggedIn
+  // if target route requires auth & no logged in user
+  // redirect to login
+  if (to.meta.auth && !loggedIn) {
+    next({ name: 'Login' })
+  } else {
+    // else then proceeds
+    next()
+  }
 })
 
 export default router
--- src/views/Profile/AuthenticatedView.vue
+++ src/views/Profile/AuthenticatedView.vue
@@ -4,5 +4,9 @@ const route = useRoute()
 </script>
 
 <template>
-  <h1>{{ route.params?.id ?? 'User' }}</h1>
+  <div>
+    <!-- change nullish to logical or
+      because param id is always povided in optional param -->
+    <h1>{{ route.params.id || 'User' }} Detail</h1>
+  </div>
 </template>
