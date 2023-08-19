2775ef562b685dfad5e4d50dc05586786b117bbb
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-08-19 08:47:19 +0700

    move input to new component

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref } from 'vue'
 import { useListStore } from '@/stores/lists'
+
+// components
+import BaseInput from '@comp/BaseInput.vue'
+
 // store container
 const store = useListStore()
 // initial input / default input
@@ -17,6 +21,8 @@ function onSubmit() {
   console.log({ ...input.value })
   // add list via store
   store.addList({ ...input.value })
+    // reset form
+  Object.assign(input, ref({ ...defaultInput }))
 }
 </script>
 
@@ -29,8 +35,8 @@ function onSubmit() {
     <!-- method handler with addList function -->
     <!-- event modifier .enter, .prevent -->
     <form class="form" @submit.prevent="onSubmit">
-      <input class="input" v-model="input.name" type="text" name="name" placeholder="John" />
-      <input class="input" v-model="input.hobby" type="text" name="hobby" placeholder="Gaming" />
+      <BaseInput class="input" v-model="input.name" name="name" placeholder="John" />
+      <BaseInput class="input" v-model="input.hobby" name="hobby" placeholder="Gaming" />
       <button type="submit">Submit</button>
     </form>
 
@@ -48,11 +54,6 @@ function onSubmit() {
 <style scoped lang="scss">
 /* body = font-size: 16px (1rem) */
 .form {
-  .input {
-    /* 2rem = 16px * 2 */
-    margin-block-end: 1rem;
-    width: 100%;
-  }
   margin-block-end: 2rem;
 }
 .list {
