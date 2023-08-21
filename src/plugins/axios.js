e6a9dbbd8d68cb7ad40044201b207dfa1fe40af2
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-08-21 23:16:52 +0700

    fix url problem

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import { getCookies, delCookies } from './cookies'
 
-const baseURL = import.meta.env.VITE_BASE_URL
+const baseURL = import.meta.env.VITE_BASE_URL??"https://be.tautan.tk"
 
 const baseApi = axios.create({
   baseURL
