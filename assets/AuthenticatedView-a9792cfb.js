0b1cc66c79c865c4d08f4890fb74d3f650ef5877
akhsan-ibrahim <akhsan-ibrahim@users.noreply.github.com>
2023-08-21 10:15:58 +0000

    deploy: 94e094615e0a5c1ccfe87aa68f5520196b7ea852

---
+++ assets/AuthenticatedView-a9792cfb.js
@@ -0,0 +1,1 @@
+import{m as t,o as a,c as s,a as r,t as o,u as n}from"./index-27778c9f.js";const p={__name:"AuthenticatedView",setup(c){const e=t();return(u,l)=>(a(),s("div",null,[r("h1",null,o(n(e).params.id||"User")+" Detail",1)]))}};export{p as default};
---
+++ assets/NotFoundView-533368e4.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-27778c9f.js";const n={};function t(r,s){return o(),c("h1",null,"404 Not Found")}const a=e(n,[["render",t]]);export{a as default};
